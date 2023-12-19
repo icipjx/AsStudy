1a64b8ebba2e0a250324d51b45ade23830393290
jx <icipjx@163.com>
2023-12-19 23:16:57 +0800

    first commit

---
+++ app/src/main/java/com/example/jia/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.jia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "jiaclick";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+
+
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.e(TAG, "onClick: ");
+//            }
+//        });
+
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Log.e(TAG, "onLongClick: " );
+                return false;
+            }
+        });
+
+        btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.e(TAG, "onTouch: "+motionEvent.getAction());
+                return false;
+            }
+        });
+
+
+    }
+
+    public void jiaClick(View view) {
+        Log.e(TAG, "onClick: ");
+    }
+}
